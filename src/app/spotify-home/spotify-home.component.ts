c3c678cfe9c2f1d4ada72abc23f58a6135b4c36d
Aman Jain <amanj0318@gmail.com>
2021-05-23 23:47:36 +0530

    User profile api added

--- src/app/spotify-home/spotify-home.component.ts
+++ src/app/spotify-home/spotify-home.component.ts
@@ -8,14 +8,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./spotify-home.component.css']
 })
 export class SpotifyHomeComponent implements OnInit {
-  
+  user: SpotifyUser = new SpotifyUser;
   constructor(private httpClient: HttpClient, 
     private router: Router) { }
 
   ngOnInit(): void {
     this.checkSession();
+    this.userProfile();
+    this.myFollowing();
+    this.myPlayList();
   }
 
+  userProfile(){
+    this.httpClient.get<any>('https://api.spotify.com/v1/me').subscribe(
+      data => {
+      console.log('user object '+data);
+      this.user  =  data;
+     
+      }
+    );
+  }
+  myFollowing(){
+    this.httpClient.get<any>('https://api.spotify.com/v1/me/following?type=artist').subscribe(
+      data => {
+      console.log('myfollowing object '+data);
+      }
+    );  
+  }
   checkSession(){
    setTimeout(() => {
     if(localStorage.getItem('access_token')===null)
@@ -27,14 +46,9 @@ export class SpotifyHomeComponent implements OnInit {
   }
   
   myPlayList() {
-    // let headers =  new HttpHeaders().set('Authorization',`Bearer ${'BQDFJmeEQsmx7DoSyb7EGzPcPq0fbdOVpKNWrd_z2X3BVb9FUzeswXIIo8mULT0p4e-IZtRfyN_mEDOL2aL7d1S9IV_32Jd7DLVR65TnGCDnchicTtO50Eq73IUT7YbPbtxX70CDfq1hXfPKA7Y7bgsvdUbrLFhXZJuGY4ZI0r3ifnAwk6s5P3u7VtGKiXtdRKPEBpK-YHT_qIK2YnDQppCpxufcRpuTg32gcoi6E9pprCp6HF2mQwE0VXBVAiu4bwVJARgsh6eHR3EHswsnjv8KVjbYYaL4FDOcDZZp'}`)
-    // .set('Accept', 'application/json')
-    // .set('Content-Type', 'application-json');
-    // console.log('Button is Clicked'+headers);
-
     this.httpClient.get<any>('https://api.spotify.com/v1/me/playlists').subscribe(
       data => {
-        alert("Process is under construction"); 
+          console.log(data);
       }
     );
   }
@@ -43,4 +57,12 @@ export class SpotifyHomeComponent implements OnInit {
     localStorage.removeItem('refresh_token');
     this.router.navigate(['']);
   }
+
+}
+
+
+export class SpotifyUser{
+display_name?: string;
+email?: string;
+followers?: string[]  = [];
 }
